1f981365edc71a6e6bde49db1544ee393ef4c8f3
Sergio <sergiolpv97@gmail.com>
2019-07-07 14:39:12 -0700

    Servidor y conexion con la base de datos creada

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+const {mongoose} = require("./database");
+//SETTINGS
+// (process.env.PORT || 3000) si el servicio de la nude te da un puerto usa ese, si no se
+// usa el que yo te doy
+app.set("port", process.env.PORT || 3000 )
+
+//MIDDLEWARES
+app.use(morgan("dev"));
+app.use(express.json());
+
+//ROUTERS
+app.use("/api/employees",require("./routers/employee.routers"));
+
+//STARING THE SERVER
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`)
+});
